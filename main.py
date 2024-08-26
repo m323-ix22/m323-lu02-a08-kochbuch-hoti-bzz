2d8c508707e4296addd78e6052e8338f706f328f
uvejs <hotiu@bzz.ch>
2024-08-26 15:09:17 +0200

    done

--- main.py
+++ main.py
@@ -1,6 +1,39 @@
-# Dein Code kommt hier hin
+"""Kochbuch"""
+
+import json
+
+def load_recipe(json_string):
+    """Lädt ein Rezept aus einem JSON-String und gibt ein Python-Dictionary zurück."""
+    return json.loads(json_string)
+
+def adjust_recipe(recipe, num_people):
+    """Passt die Mengenangaben im Rezept an die gegebene Anzahl von Personen an."""
+    factor = num_people / recipe['servings']
+    adjusted_ingredients = {
+        ingredient: round(amount * factor, 2)  # Rundet die Menge auf 2 Dezimalstellen
+        for ingredient, amount in recipe['ingredients'].items()
+    }
+    # Erstelle ein neues Rezept-Dictionary mit den angepassten Mengen
+    adjusted_recipe = {
+        'title': recipe['title'],
+        'ingredients': adjusted_ingredients,
+        'servings': num_people
+    }
+    return adjusted_recipe
 
 if __name__ == '__main__':
     # Beispiel für die Datenstruktur eines Rezepts
     recipe_json = '{"title": "Spaghetti Bolognese", "ingredients": {"Spaghetti": 400, "Tomato Sauce": 300, "Minced Meat": 500}, "servings": 4}'
-    # Dein Code kommt hier hin
+
+    # Rezept laden
+    recipe = load_recipe(recipe_json)
+    print("Original Recipe:", recipe)
+
+    # Rezept für 2 Personen anpassen
+    adjusted_recipe = adjust_recipe(recipe, 2)
+    print("Adjusted Recipe for 2 servings:", adjusted_recipe)
+
+    # Rezept für 6 Personen anpassen
+    adjusted_recipe = adjust_recipe(recipe, 6)
+    print("Adjusted Recipe for 6 servings:", adjusted_recipe)
+
